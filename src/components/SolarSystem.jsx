a0c421b2652eabc7be3c5c554082fd9143855c16
Rafael Souza <souza_rafael.97@outlook.com>
2022-07-20 16:22:16 -0300

    Adiciona Requisito 4

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class SolarSystem extends Component {
   render() {
     return (
-      <div data-testid="solar-system">SolarSystem</div>
+      <div data-testid="solar-system">
+        <Title headline="planetas" />
+      </div>
     );
   }
 }
