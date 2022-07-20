f0d98dc040ef5f6ebc382eb66d2ed87d61f451c1
Rafael Souza <souza_rafael.97@outlook.com>
2022-07-20 17:05:00 -0300

    Adiciona Requisito 5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
+import PlanetCard from './components/PlanetCard';
 
 class App extends React.Component {
   render() {
@@ -9,7 +9,7 @@ class App extends React.Component {
       <div>
         <Header />
         <SolarSystem />
-        <Title />
+        <PlanetCard />
       </div>
     );
   }
