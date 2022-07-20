fc53dcd873f9ef8e4c490d7a9082d2057ecb7125
Rafael Souza <souza_rafael.97@outlook.com>
2022-07-20 18:10:06 -0300

    Adiciona Requisito 8

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions">Missions</div>
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
     );
   }
 }
