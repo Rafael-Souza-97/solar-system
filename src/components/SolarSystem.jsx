c0eccea06e061542fa76a5df871c6568da8c2665
Rafael Souza <souza_rafael.97@outlook.com>
2022-07-20 18:00:35 -0300

    Adiciona Requisito 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="planetas" />
+        {planets.map((planet, index) => (<PlanetCard
+          key={ index }
+          planetName={ planet.name }
+          planetImage={ planet.image }
+        />))}
       </div>
+
     );
   }
 }
